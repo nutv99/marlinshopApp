873e477289120cfbe8654067bc37eb694d0101ac
nutv99 <nutv99@gmail.com>
2019-06-23 16:58:38 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,39 +1,4 @@
-<!doctype html>
-<html lang="en">
- <head>
-  <meta charset="UTF-8">
-  <meta name="Generator" content="EditPlus®">
-  <meta name="Author" content="">
-  <meta name="Keywords" content="">
-  <meta name="Description" content="">
-  <title>HTML Template</title>
+<?php    
+    echo "This--> Marlin Bot";
 
-  <link rel="shortcut icon" href="http://www.hdlightcatcherz.com/celikashop/templates/orajisjoomla/favicon.ico" />
-
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-<!-- Optional theme -->
-<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-<!-- Latest compiled and minified JavaScript -->
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
-
-<!-- My Declare Style  -->
-
-<link rel="stylesheet" href="nutv99style.css" >
-  <style>
-  
-
-
-  </style>
-
-
- </head>
- <body>
-    
-This isApp Marlin Shop
- </body>
-</html>
+?> 
